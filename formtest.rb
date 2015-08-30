d3ea0f18cf4ff3d9a400b4b099de468614dbf877
De Wet Blomerus <dewet@blomerus.org>
2015-08-30 14:59:15 +0200

    initial commit

---
+++ formtest.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'mechanize'
+
+login_url = "http://blomerusphotography.com/contact/"
+agent = Mechanize.new
+agent.user_agent_alias = 'Mac Safari'
+page = agent.get(login_url)
+form = page.forms.first
+# form.fields.each { |f| puts f.name }
+form.full_name = 'Formtest Form Filler'
+form.email = 'dewet@blomerus.org'
+form.city = 'Cape Town'
+form.message = "I am a computer so I will not be having children until 2030 at the earliest"
+page = form.submit
+puts page.body
