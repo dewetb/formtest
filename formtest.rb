90a2e99cce0d8a683a01e8aafb418bd07cbb7a2e
De Wet Blomerus <dewet@blomerus.org>
2015-10-15 12:58:42 +0200

    fixed form field name not allowed as method name

--- formtest.rb
+++ formtest.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/home/dewet/.rbenv/shims/ruby
 require 'rubygems'
 require 'mechanize'
 
@@ -6,11 +6,12 @@ login_url = "http://blomerusphotography.com/contact/"
 agent = Mechanize.new
 agent.user_agent_alias = 'Mac Safari'
 page = agent.get(login_url)
-form = page.forms.first
-# form.fields.each { |f| puts f.name }
+form = page.forms[0]
+
+#form.fields.each { |f| puts f.name }
 form.full_name = 'Formtest Form Filler'
 form.email = 'dewet@blomerus.org'
 form.city = "Let's just say I'm on the internet"
-form.message = "I am a computer so I will not be having children until 2030 at the earliest"
+form.send 'message-new=', "I am a computer so I will not be having children until 2030 at the earliest"
 page = form.submit
 # puts page.body
